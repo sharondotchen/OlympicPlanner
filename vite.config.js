732ee69081b28403b27831b2a1a4870b351e83f3
juliana-george <george.ju@northeastern.edu>
2025-04-02 13:39:41 -0400

    troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,8 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: 'OlympicPlanner',
-  plugins: [svelte()],
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true
+export default {
+  optimizeDeps: {
+    exclude: ['fsevents']
   }
-})
+};
