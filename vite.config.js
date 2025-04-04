1b7680576744947dd27a1801effc53c75ed077b6
sharondotchen <chen.sharo@northeastern.edu>
2025-04-03 23:13:40 -0400

    conifg

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'OlympicPlanner',
+  base: '/OlympicPlanner/',
   plugins: [svelte()],
   build: {
     outDir: 'docs',
