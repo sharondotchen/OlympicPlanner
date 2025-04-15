5bf649a2ce7fe1eee4305c23d6fd653d8354eeb6
juliana-george <george.ju@northeastern.edu>
2025-04-14 22:01:18 -0400

    final styling changes

--- src/lib/4Choice.svelte
+++ src/lib/4Choice.svelte
@@ -7,11 +7,11 @@
 <div id="disaster-picker">
     <button class="cancelled">You chose to cancel.</button>
     <div id = "not-cancelled">
-    <p>Some Townsvillians are sad to miss out on the yearly tradition, but the people who lost homes or businesses to the fire are grateful to be the focus of the town's recovery efforts.
-        <br><br>
-        The grass-roots organization NOlympics LA would agree with your decision. 
-        <br><br>
-        Since the International Olympic Committee <a href="https://www.olympics.com/ioc/2024-2028-host-city-election">awarded</a> Los Angeles the 2028 Olympic bid in 2017, the activist group NOlympics LA has been calling for its cancellation, citing LA's already-dire housing crisis as well as the <a href="https://nolympicsla.com/2020/06/29/examining-the-la-1984-olympic-legacy-capitalism-police-violence-and-privatization/">harmful legacy</a> of the 1984 Games on racialized policing and mass incarceration. NOlympics LA has <a href="https://nolympicsla.com/2025/01/15/let-the-olympics-burn-not-la/">renewed</a> their activism with even more urgency after this year's <a href="https://www.nbcnews.com/news/us-news/california-wildfires-what-we-know-palisades-eaton-los-angeles-rcna188239">devastating fires</a> — unlike in Townsville, the LA fires resulted in 29 confirmed casualties, thousands of destroyed buildings, and an unknown number of displaced Angelenos. Also unlike Townsville, in LA, taxpayers are <a href="https://www.nytimes.com/2024/08/12/business/economy/olympics-los-angeles-2028-economy.html#:~:text=LA28%2C%20the%20private%20group%20organizing,the%20opening%20and%20closing%20ceremonies.">liable</a> for any Olympic-related expenses should LA28 run over the $7 billion budget (which, based on recent Games, is likely). 
+    <p>Some Townsvillians are sad to miss out on the yearly tradition, but the people who lost homes or businesses to the fire are grateful to be the focus of the town's recovery efforts.</p>
+       
+        <p class="highlight">NOlympics LA would agree with your decision.</p> 
+      
+       <p> Since the International Olympic Committee <a href="https://www.olympics.com/ioc/2024-2028-host-city-election">awarded</a> Los Angeles the 2028 Olympic bid in 2017, the activist group NOlympics LA has been calling for its cancellation, citing LA's already-dire housing crisis as well as the <a href="https://nolympicsla.com/2020/06/29/examining-the-la-1984-olympic-legacy-capitalism-police-violence-and-privatization/">harmful legacy</a> of the 1984 Games on racialized policing and mass incarceration. NOlympics LA has <a href="https://nolympicsla.com/2025/01/15/let-the-olympics-burn-not-la/">renewed</a> their activism with even more urgency after this year's <a href="https://www.nbcnews.com/news/us-news/california-wildfires-what-we-know-palisades-eaton-los-angeles-rcna188239">devastating fires</a> — unlike in Townsville, the LA fires resulted in 29 confirmed casualties, thousands of destroyed buildings, and an unknown number of displaced Angelenos. Also unlike Townsville, in LA, taxpayers are <a href="https://www.nytimes.com/2024/08/12/business/economy/olympics-los-angeles-2028-economy.html#:~:text=LA28%2C%20the%20private%20group%20organizing,the%20opening%20and%20closing%20ceremonies.">liable</a> for any Olympic-related expenses should LA28 run over the $7 billion budget (which, based on recent Games, is likely). 
         <br><br>
         Despite these serious concerns, shared by a <a href="https://www.latimes.com/california/story/2023-03-21/poll-is-la-ready-for-the-olympics">statistically significant</a> number of Angelenos, thus far city officials have given no indication that they are considering cancelling the Olympics. LA Mayor Karen Bass' fire response efforts have also <a href="https://www.latimes.com/california/story/2025-03-11/la-me-fire-poll-bass-newsom">left much to be desired</a>, and the fires have already <a href="https://apnews.com/article/los-angeles-wildfires-tenants-rent-housing-819c58a2a111bf97fccd3a5dfe6b66e7">exacerbated</a> the existing problems with LA's brutal rental market. NOlympics LA organizers argue that the city should cancel the Games and instead focus on protecting victims of the fire from displacement and homelessness; hosting the Olympics would only contribute to further destruction. </p>
     </div>
@@ -20,3 +20,17 @@
 <br><br><br>
 <button class="navbutton" onclick={()=> goToSlide(1)}>↻ Retake Quiz</button>
 <button class="navbutton" onclick={()=> goToSlide(9)}>Next →</button>
+
+<style>
+   @keyframes highlight{
+    from {background-position: 0}
+    to {background-position: -100%}
+}
+.highlight{
+  animation-name: highlight;
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
+  background-size: 196%;
+  background-image: linear-gradient(to right, transparent 50%, var(--accent) 50%)
+}
+</style>
--- src/lib/4ChoiceC.svelte
+++ src/lib/4ChoiceC.svelte
@@ -7,10 +7,11 @@
     <button class="cancelled">You chose to cancel.</button>
     <div id = "not-cancelled">
     <p>Some Townsvillians are sad to miss out on the yearly tradition, but health experts and Townsvillians with immunocompromised friends and family praise the decision.
-        <br><br>
-        According to a <a href="https://www.nytimes.com/2021/05/18/world/asia/covid-japan-olympics-poll.html">2021 poll</a>, 83% of the Japanese public would also agree with your decision.<br><br>
+      
+      <p class="highlight">  According to a <a href="https://www.nytimes.com/2021/05/18/world/asia/covid-japan-olympics-poll.html">2021 poll</a>, 83% of the Japanese public would agree with your decision.</p>
         
-        The 2021 Tokyo Olympics, originally slated for 2020, were postponed for a year due to the COVID-19 pandemic. However, as the Games approached, nine prefectures including Tokyo were still under a <a href="https://www.nytimes.com/2021/05/07/world/asia/japan-covid-emergency-olympics.html">state of emergency</a>, and citizens and medical professionals alike urged the Games' cancellation. Anti-Olympic activist group Hangorin No Kai (反五輪の会) also <a href="https://prismreports.org/2024/09/05/los-angeles-olympics-protests/">invoked</a> the harmful legacy of displacement from the 1964 Games, highlighting the stories of roughly 200 families who were <a href="https://www.reuters.com/world/asia-pacific/sad-lonely-feeling-tokyo-man-evicted-twice-50-years-apart-olympic-construction-2021-07-01/">evicted</a> from public housing in 2013 due to Olympics-related construction — several of whom had also <a href="https://www.reuters.com/world/asia-pacific/sad-lonely-feeling-tokyo-man-evicted-twice-50-years-apart-olympic-construction-2021-07-01/">faced</a> eviction ahead of the 1964 Olympics. Unlike Townsville, Japan had a significant aging population, a severe shortage of medical professionals, and only an <a href="https://www.nbcnews.com/news/olympics/ahead-tokyo-olympics-japan-vaccination-rate-far-mark-n1272523">8%</a> vaccination rate just one month away from the Games (compared to 45% in the U.S. and 10% per country on average worldwide).
+        <p>The 2021 Tokyo Olympics, originally slated for 2020, were postponed for a year due to the COVID-19 pandemic. However, as the Games approached, nine prefectures including Tokyo were still under a <a href="https://www.nytimes.com/2021/05/07/world/asia/japan-covid-emergency-olympics.html">state of emergency</a>, and citizens and medical professionals alike urged the Games' cancellation. Anti-Olympic activist group Hangorin No Kai (反五輪の会) also <a href="https://prismreports.org/2024/09/05/los-angeles-olympics-protests/">invoked</a> the harmful legacy of displacement from the 1964 Games, highlighting the stories of roughly 200 families who were <a href="https://www.reuters.com/world/asia-pacific/sad-lonely-feeling-tokyo-man-evicted-twice-50-years-apart-olympic-construction-2021-07-01/">evicted</a> from public housing in 2013 due to Olympics-related construction — several of whom had also <a href="https://www.reuters.com/world/asia-pacific/sad-lonely-feeling-tokyo-man-evicted-twice-50-years-apart-olympic-construction-2021-07-01/">faced</a> eviction ahead of the 1964 Olympics. 
+        <br><br> Unlike Townsville, Japan had a significant aging population, a severe shortage of medical professionals, and only an <a href="https://www.nbcnews.com/news/olympics/ahead-tokyo-olympics-japan-vaccination-rate-far-mark-n1272523">8%</a> vaccination rate just one month away from the Games (compared to 45% in the U.S. and 10% per country on average worldwide).
         <br><br>
         Despite these serious concerns, the 2021 Olympics proceeded as planned. As expected, COVID cases <a href="https://www.washingtonpost.com/world/2021/08/06/japan-covid-surge-olympics/">surged</a> to record rates during the Games. In fact, the Japanese government <a href="https://www.yahoo.com/news/tokyo-covered-arrival-deadly-covid-103011468.html?guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAAZtATQ50MiCzjZagurcjoL2TtS7Kid_9JIPYQrOu8TWfQ96xn9tOLyRXJabNo2ZmbTW0nBIlnc6Y7pWhAW6YNdLeeZugMsbrAnBpKQG87W8OXG5t5MLwI4lIdFH_NSL5QnCyKygYJsAloQH0zZ7ZEaffuznEhzzpRKzPlzxUDS_">deliberately delayed announcing</a> a deadly new COVID variant because of the Olympics. Furthermore, half a million potentially life-saving COVID tests <a href="https://apnews.com/article/2020-tokyo-olympics-lifestyle-technology-sports-japan-olympic-team-0645e7853f6b152729ba80f332cd0fa7">flowed</a> into the Olympic village throughout the two-week duration of the Olympics, while the Japanese public confronted testing kit shortages. Although International Olympic Committee President Thomas Bach <a href="https://www.theguardian.com/sport/2021/aug/06/thomas-bach-praises-soul-of-tokyo-olympics-but-faces-twitter-backlash">called</a> Tokyo “the best-ever prepared Olympic city,” for the most part, the Japanese public vehemently disagreed. </p>
     </div>
@@ -19,3 +20,17 @@
 <br><br><br>
 <button class="navbutton" onclick={()=> goToSlide(1)}>↻ Retake Quiz</button>
 <button class="navbutton" onclick={()=> goToSlide(9)}>Next →</button>
+
+<style>
+    @keyframes highlight{
+     from {background-position: 0}
+     to {background-position: -100%}
+ }
+ .highlight{
+   animation-name: highlight;
+   animation-duration: 1s;
+   animation-fill-mode: forwards;
+   background-size: 196%;
+   background-image: linear-gradient(to right, transparent 50%, var(--accent) 50%)
+ }
+ </style>
